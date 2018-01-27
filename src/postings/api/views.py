99065bbf5e4c9f05647c877f22e49b2ca7fa0ae9
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2018-01-27 13:25:46 -0700

    Create mi first API endpoint (with Retrieve Update Destroy generic view)

---
+++ src/postings/api/views.py
@@ -0,0 +1,18 @@
+# generic 
+
+from rest_framework import generics
+from postings.models import BlogPost
+
+from .serializers import BlogPostSerializer
+
+class BlogPostRudView(generics.RetrieveUpdateDestroyAPIView):
+    lookup_field    = 'pk' #slug, id
+    serializer_class     =  BlogPostSerializer
+    #queryset        = BlogPost.objects.all()
+
+    def get_queryset(self):
+        return BlogPost.objects.all()
+
+    # def get_object(self):
+    #     pk  = self.kwargs.get("pk")
+    #     return BlogPost.objects.get(pk=pk)
