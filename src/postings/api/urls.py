eab7dda3abd876c9b8997e159aeaf6a252139b53
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2018-01-27 13:50:07 -0700

    Add API endpoint to create new post

--- src/postings/api/urls.py
+++ src/postings/api/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from .views import BlogPostRudView
+from .views import BlogPostRUDView, BlogPostAPIView
 
 urlpatterns = [
-    url(r'^(?P<pk>\d+)/$', BlogPostRudView.as_view(), name='post-rud')
+    url(r'^(?P<pk>\d+)/$', BlogPostRUDView.as_view(), name='post-rud'),
+    url(r'^$', BlogPostAPIView.as_view(), name='post-create')
 ]
--- src/postings/api/views.py
+++ src/postings/api/views.py
@@ -5,7 +5,20 @@ from postings.models import BlogPost
 
 from .serializers import BlogPostSerializer
 
-class BlogPostRudView(generics.RetrieveUpdateDestroyAPIView):
+class BlogPostAPIView(generics.CreateAPIView):
+    lookup_field    = 'pk' #slug, id
+    serializer_class     =  BlogPostSerializer
+    #queryset        = BlogPost.objects.all()
+
+    def get_queryset(self):
+        # qs = BlogPost.objects.all()
+        # qs = qs.exclude(pk=2)
+        return BlogPost.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class BlogPostRUDView(generics.RetrieveUpdateDestroyAPIView):
     lookup_field    = 'pk' #slug, id
     serializer_class     =  BlogPostSerializer
     #queryset        = BlogPost.objects.all()
